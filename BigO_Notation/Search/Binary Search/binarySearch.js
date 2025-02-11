b8d39c2205e08770611b1e8c39710613693baba4
Jaimin1999 <learnwithjaimin@gmail.com>
2025-02-11 17:02:48 +0530

    JavaScript Algorithms - 16 - Binary Search

---
+++ BigO_Notation/Search/Binary Search/binarySearch.js
@@ -0,0 +1,21 @@
+arr = [3, 44, 45, 100, 102];
+
+function binarySearch(n, t) {
+  let min = 0;
+  let max = n.length - 1;
+
+  while (min <= max) {
+    let mid = Math.floor((min + max) / 2);
+    if (n[mid] === t) return mid;
+
+    if (n[mid] > t) {
+      max = mid - 1;
+    }
+    if (n[mid] < t) {
+      min = mid + 1;
+    }
+  }
+  return -1;
+}
+
+console.log(binarySearch(arr, 102));
